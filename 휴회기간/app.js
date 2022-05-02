bb974fb8c612e6efaafeacc044e0be09f98c49f7
BeomSoon Chung <joseph000201@gmail.com>
2022-05-02 19:29:27 +0900

    .

--- 휴회기간/app.js
+++ 휴회기간/app.js
@@ -9,69 +9,76 @@
 // document.getElementById('close').addEventListener('click', () =>document.getElementById('alert').style.display = 'none')
 // document.getElementById('close').addEventListener('click', () =>document.getElementById('alert').style.display = 'none')
 
-$('.navbar-toggler').on('click', () => $('.list-group').toggleClass('show-overflow'));
+
+// $('.navbar-toggler').on('click', () => $('.list-group').toggleClass('show-overflow'));
 
 
 $('.openModal').on('click', () => $('.black-bg').addClass('show-opacity'));
-
 $('#close').on('click', () => $('.black-bg').removeClass('show-opacity'));
+//modal창에 animation 주기
 
-function checkInputIsNull (e){
-    let email = $('.form-control').eq('0').val();
-    let password = $('.form-control').eq('1').val();
-    if(email ==='' || !(/\S+@+\S+\.\S+/.test(email))){
-        alert('Input your email!');
-        e.preventDefault();
-        console.log(email);
+
+let validText = {
+    checkInputIsNull: function (e){
+        const EMAIL = $('.form-control').eq('0').val();
+        const PASSWORD = $('.form-control').eq('1').val();
+        if(EMAIL ==='' || !(/\S+@+\S+\.\S+/.test(EMAIL))){
+            alert('Input your email!');
+            e.preventDefault();
+            console.log(EMAIL);
+        }
+        else if(PASSWORD ==='' || /[A-Z]/.test(PASSWORD)){
+            alert('Input your password!');
+            e.preventDefault();
+    
+        }
+    
+    },
+    passwordCheck : function (){
+        const PASSWORD = $('.form-control').eq('1');
+        if(PASSWORD.val().length < 7){
+            PASSWORD.on('input', ()=>$('.form-text').eq('1').removeClass('none'));
+        }
+        else{
+            PASSWORD.on('input', ()=>$('.form-text').eq('1').addClass('none'));
+        }
     }
-    else if(password ==='' || /[A-Z]/.test(password)){
-        alert('Input your password!');
-        e.preventDefault();
-
-    }
-
 }
 
-function passwordCheck(){
-    let password = $('.form-control').eq('1');
-    if(password.val().length < 7){
-        password.on('input', ()=>$('.form-text').eq('1').removeClass('none'));
-    }
-    else{
-        password.on('input', ()=>$('.form-text').eq('1').addClass('none'));
+let darkMode = {
+    count : 0,
+    Toggler: function(){
+        if(count%2 === 0){
+            $('.badge').html('Light');
+            count++;
+        }
+        else{
+            $('.badge').html('Dark');
+            count--;
+        }
+        $('body').toggleClass('dark');
     }
 }
 
-let count = 0;
 
-function darkToggler(){
-    if(count%2 === 0){
-        $('.badge').html('Light');
-        count++;
-    }
-    else{
-        $('.badge').html('Dark');
-        count++;
-    }
-    $('body').toggleClass('dark');
-
-}
 function scrollEndChecker (){
-    let scrollAmount = $('.lorem').scrollTop();
-    let actualHeight = $('.lorem').prop('scrollHeight');
-    let height = $('.lorem').prop('clientHeight');
+    const SCROLL_AMOUNT = $('.lorem').scrollTop();
+    const ACTUAL_HEIGHT = $('.lorem').prop('scrollHeight');
+    const HEIGHT = $('.lorem').prop('clientHeight');
 
-    if((actualHeight-scrollAmount-height) < 0.1){
+    if((ACTUAL_HEIGHT - SCROLL_AMOUNT - HEIGHT) < 0.1){
         alert('다 읽었군요?');
     }
-    console.log(scrollAmount, actualHeight, height);
+    console.log(SCROLL_AMOUNT, ACTUAL_HEIGHT, HEIGHT);
 }
 
 
-$('form').on('submit', checkInputIsNull);
-$('.form-control').eq('1').on('input',passwordCheck);
+$('form').on('submit', validText.checkInputIsNull);
+$('.form-control').eq('1').on('input',validText.passwordCheck);
+//modal창에서 제출된 정보 유효성 검사
 
-$('.badge').on('click', darkToggler);
+$('.badge').on('click', darkMode.Toggler);
+//dark모드
 
 $(window).on('scroll',  function () {
     if ($(window).scrollTop() > 100){
@@ -84,49 +91,58 @@ $(window).on('scroll',  function () {
 );
 $('.lorem').on('scroll', scrollEndChecker);
 
-let timer = 5;
-function timerOfFreeGift (){
-    timer -= 1;
-    $('.alert-danger').html(timer + "초 이내 구매 시 사은품 증정!");
-    if (timer <1){
-        clearInterval(freeGiftTimer);
-        $('.alert-danger').addClass('none');
-    }
-    console.log(timer);
-}
-
-let freeGiftTimer = setInterval(timerOfFreeGift, 1000);
-
-
-
+let count = 5;
+    function freeGiftOfTimer(){
+        count--;
+        $('.alert-danger').html(count + "초 이내 구매 시 사은품 증정!");
+        if (count <1){
+            clearInterval(TIMER);
+            $('.alert-danger').addClass('none');
+        }
+    }  
+    const TIMER = setInterval(freeGiftOfTimer, 1000);
+    
+    
+    
+    
+    
 let carouselNum = 0;
 function carouselNextButton (){
-    carouselNum++;
-    let num = -100 * carouselNum;
-    $('html').css('--num', `translateX(${num}vw)`);
+    if (0<carouselNum<2){
+        carouselNum++;
+        let num = -100 * carouselNum;
+        $('html').css('--num', `translateX(${num}vw)`);       
+    }
 }
 function carouselBeforeButton (){
-    carouselNum--;
-    let num = -100 * carouselNum;
-    $('html').css('--num', `translateX(${num}vw)`);
-
+    if (-1<carouselNum<2){
+        carouselNum--;
+        let num = -100 * carouselNum;
+        $('html').css('--num', `translateX(${num}vw)`);
+    }
+    
 }
 
 
-$('.slide-2').on('click', () => {
-$('.slide-container').css('transform', 'translateX(-100vw)');
-carouselNum++
-}
-);
 
 $('.slide-1').on('click', () =>{
-$('.slide-container').css('transform', 'translateX(0vw)');
-carouselNum = 0;
+    carouselNum = 0;
+    let num = carouselNum * -100;
+    
+    $('html').css('--num', `translateX(${num}vw)`);
 }
 );
+$('.slide-2').on('click', () => {
+    carouselNum = 1;
+    let num = carouselNum * -100;
+    $('html').css('--num', `translateX(${num}vw)`);
+}
+);
+
 $('.slide-3').on('click', ()=> {
-$('.slide-container').css('transform', 'translateX(-200vw)');
-carouselNum++;
+    carouselNum = 2;
+    let num = carouselNum * -100;
+    $('html').css('--num', `translateX(${num}vw)`);
 }
 );
 
